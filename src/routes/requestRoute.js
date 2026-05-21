afceb98edbc8815d54340c8f622fd78a67dd94b8
Saikrishnabalusu <saikrishnabalusu97@gmail.com>
2026-05-21 16:54:45 +0530

    Add pending requests route to fetch user details for interested connections

--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/userAuth");
 const { ConnectionModel } = require("../models/connectionModel")
 const requestRoute = express.Router();
+const { User } = require("../models/userModel"); ``
 
 requestRoute.post("/review/:status/:toUserId", userAuth, async (req, res) => {
     try {
@@ -30,6 +31,29 @@ requestRoute.post("/review/:status/:toUserId", userAuth, async (req, res) => {
 
 })
 
+requestRoute.get("/pendingRequests", userAuth, async (req, res) => {
+    try {
+        const loginUser = req.loginUser;
+        const pendingRequests = await ConnectionModel.find({ $or: [{ fromUserId: loginUser._id }, { toUserId: loginUser._id }], status: "interested" })
+        // console.log("pendingRequests...", pendingRequests)
+        const pendingRequestsUserIds = pendingRequests.map(request => {
+            if (request.fromUserId.toString() === loginUser._id.toString()) {
+                return request.toUserId;
+            }
+            else {
+                return request.fromUserId;
+            }
+        })
+        // console.log("pendingRequestsUserIds...", pendingRequestsUserIds)
+        const safeDisplayInfo = await User.find({ _id: { $in: pendingRequestsUserIds } }, "firstName lastName age gender skills profileUrl");
+        res.json({
+            message: "Pending Requests Fetched Successfully",
+            data: safeDisplayInfo
+        });
 
+    } catch (error) {
+        res.status(500).send("Something went wrong in Pending Requests Router - " + error.message)
+    }
+})
 
 module.exports = { requestRoute }
